2fd7a0d14bbd52b94d2e46bb5c300781ca271b50
milesr100 <100169944+milesr100@users.noreply.github.com>
2022-12-15 21:01:10 -0500

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,5 +3,12 @@ import pandas as pd
 import os
 
 
+MYSQL_HOSTNAME = os.getenv("MYSQL_HOSTNAME")
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+
+
 gators = pd.read_csv('data/fatal_alligator_attacks_US.csv')
 gators.to_sql('fatal_alligator_attacks_US', con=engine, if_exists= 'replace')
